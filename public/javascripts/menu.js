2289f74c0c3fc9d8c41b0810dda9f9a0f0bd73e0
fanlan1210 <fanlan1210@users.noreply.github.com>
2020-11-22 05:05:34 +0800

    update shop menu template

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -1,25 +1,49 @@
-let foodCount = 0;
+let foodCount = {};
 
-$("#food").click(()=>{
-  $("#foodSel").text($("#food h5").text());
-  foodCount++;
-  $("#nums").val(foodCount);
-})
+//$(".card-deck a").click(()=>{
+//  $("#foodSel").append(`<p>${$(this).text()}</p>`);
+//  
+//  if(foodCount[$("card h5").text()] !== undefined) foodCount[$("card h5").text()]++;
+//  else foodCount[$("card h5").text()] = 0;
+//  
+//  //$("#nums").val(foodCount);
+//})
 
-$("#add").click(()=>{
-  foodCount++;
-  $("#nums").val(foodCount);
-})
-
-$("#remove").click(()=>{
-  if(foodCount>0) foodCount--;
-  $("#nums").val(foodCount);
-})
-
-$("#nums").change(()=>{
-  foodCount = $("#nums").val();
-  if(foodCount<0){
-    foodCount = 0;
-    $("#nums").val("0");
+function order(vary,price){
+  if(foodCount[vary] !== undefined) foodCount[vary]++;
+  else {
+    foodCount[vary] = 1;
+    $("#foodSel").append(`<div class="float-right"><div class="input-group"><div class="input-group-prepend"><button class="btn btn-secondary" onclick="increment('${vary}')">+</button></div><span class="input-group-text" id="${vary}"></span><div class="input-group-append"><button class="btn btn-secondary" onclick="decrement('${vary}')">-</button></div></div></div><p>${vary}</p>`);
   }
-})
+  
+  $(`#${vary}`).text(foodCount[vary]);
+}
+
+function increment(vary){
+  foodCount[vary] += 1;
+  $(`#${vary}`).text(foodCount[vary]);
+}
+
+function decrement(vary){
+  if(foodCount[vary]>1) foodCount[vary] -= 1;
+  $(`#${vary}`).text(foodCount[vary]);
+}
+
+
+//$("#add").click(()=>{
+//  foodCount++;
+//  $("#nums").val(foodCount);
+//})
+//
+//$("#remove").click(()=>{
+//  if(foodCount>0) foodCount--;
+//  $("#nums").val(foodCount);
+//})
+
+//$("#nums").change(()=>{
+//  foodCount = $("#nums").val();
+//  if(foodCount<0){
+//    foodCount = 0;
+//    $("#nums").val("0");
+//  }
+//})
